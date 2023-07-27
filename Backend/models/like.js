4d9e745df068077aba39517e6c67e1784f18b352
Nir y <Niry27@gmail.com>
2023-07-27 18:53:23 +0300

    Clean some more files back

--- Backend/models/like.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const postSchema = mongoose.Schema({
-    postId: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
-    userLiked: { type: mongoose.Schema.Types.ObjectId, ref: "User", require: true },
-});
-
-module.exports = mongoose.model("Like", postSchema);
