6bb1a4a7d4872420409cff6c9ade8968eb21818f
Nir y <Niry27@gmail.com>
2023-08-03 07:26:31 +0300

    Category start to work on

---
+++ Backend/controllers/category.js
@@ -0,0 +1,27 @@
+
+const Category = require("../models/category");
+
+
+
+exports.createCategory = (req, res, next) => {
+    res.status(400).json({
+        message: "Not Implemented!",
+        error: "n/a"
+    });
+}
+
+
+exports.getCategory = (req, res, next) => {
+    res.status(400).json({
+        message: "Not Implemented!",
+        error: "n/a"
+    });
+}
+
+
+exports.getAllCategories = (req, res, next) => {
+    res.status(400).json({
+        message: "Not Implemented!",
+        error: "n/a"
+    });
+}
